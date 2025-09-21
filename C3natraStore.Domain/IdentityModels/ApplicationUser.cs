1d07a2d05aab338c61de7a0a26d4f6fd7bf0e002
bojanzavojchevski <bojannn890@gmail.com>
2025-09-21 04:14:26 +0200

    Add project files.

---
+++ C3natraStore.Domain/IdentityModels/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3natraStore.Domain.IdentityModels
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required(ErrorMessage = "Required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Required")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Required")]
+        public string ShippingAddress { get; set; }
+    }
+}
---
+++ C3natraStore.Repository/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using C3natraStore.Domain.IdentityModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace C3natraStore.Repository.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
